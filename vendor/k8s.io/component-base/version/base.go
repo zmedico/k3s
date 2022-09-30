7e2a84b5f5ec989ca0f237364e43f46da0b4a99a
Zac Medico <zachary.medico@sony.com>
2022-09-29 18:19:13 -0700

    vendor: import v1.25.2+k3s1 dependencies

---
+++ vendor/k8s.io/component-base/version/base.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	gitMajor = "1"
+	gitMinor = "25"
+	gitVersion   = "v1.25.2-k3s1"
+	gitCommit    = "90b9bd8f5e41f3ffd4bdae40f3cd73259365daa4"
+	gitTreeState = "clean"
+	buildDate = "2022-09-22T00:00:41Z"
+)
